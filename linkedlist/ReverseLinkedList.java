09e77fcd8a75beaaec407bceced83b2af1a71a83
Akshaya-K-SJCE <akshayakannan0910@gmail.com>
2025-12-18 21:58:45 +0530

    Implement reverse and print methods for linked list
    Added methods to reverse a linked list and print it.

--- linkedlist/ReverseLinkedList.java
+++ linkedlist/ReverseLinkedList.java
@@ -1,1 +1,36 @@
+public class ReverseLinkedList {
+    public static Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+        while(current != null) {
+            Node next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+
+    public static void printList(Node head) {
+        Node temp = head;
+        while(temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+
+        System.out.println("Original List:");
+        printList(head);
+
+        head = reverse(head);
+        System.out.println("Reversed List:");
+        printList(head);
+    }
+}
 
